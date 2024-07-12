e4f89424e7475a60edda415b05ab7f8a79c2ba07
emrahhorsunlu <73930484+emrahhorsunlu@users.noreply.github.com>
2024-07-12 21:27:02 +0300

    First Commit

--- obj/Debug/net5.0/UserManagement.API.AssemblyInfo.cs
+++ obj/Debug/net5.0/UserManagement.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd427c457333a8fa794b89a926b5a42041174ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
